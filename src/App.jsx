47adb9434b2165ae60e99d1c32da6942435d77cc
levi <ajdf@jahd.com>
2024-11-10 20:08:34 -0500

    full project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,59 @@
-import React, { useState } from "react";
-import "./App.css";
-import ConferenceEvent from "./ConferenceEvent";
-import AboutUs from "./AboutUs";
+    import { createSlice } from "@reduxjs/toolkit";
 
-function App() {
-  const [showVenue, setShowVenue] = useState(false);
+export const avSlice = createSlice({
+  name: "av",
+  initialState: [
+        {
+        img: "",
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: "",
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: "",
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: "",
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
 
-  const handleGetStarted = () => {
-    setShowVenue(true);
-  };
+    {
+        img: "",
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
 
-  return (
-    <>
-      <header className="first_page">
-        <div className="main_event">
-          <div className="first_page_name_btn">
-            <h1 className="budget_heading">Conference Expense Planner</h1>
-            <p className="budget_sentence"> Plan your next major event with us!</p>
-            <div className="getstarted_btn">
-              <button onClick={() => handleGetStarted()} className="get-started-btn">
-                Get Started
-              </button>
-            </div>
-          </div>
-          <div className="aboutus_main">
-            <AboutUs />
-          </div>
-        </div>
-      </header>
+  ],
 
-      <div className={`event-list-container ${showVenue ? 'visible' : ''}`}>
-        <ConferenceEvent />
-      </div>
-    </>
-  );
-}
 
-export default App;
+  reducers: {
+    incrementAvQuantity: (state, action) => {
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
+    },
+    decrementAvQuantity: (state, action) => {
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
+    },
+  },
+});
+
+export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
+
+export default avSlice.reducer;
